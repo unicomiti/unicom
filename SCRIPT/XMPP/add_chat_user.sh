593d0028e5340ba772d65a94a7403b2cbae38686
Maxime Lauret <mlauret@intechinfo.fr>
2015-06-24 14:14:48 +0200

    Ajout script add_chat_user

---
+++ SCRIPT/XMPP/add_chat_user.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# LAURET Maxime
+# Adding users to the Prosody server
+
+
+# TEMP : À terme, les variables seront composées des champs du site.  Pour l'instant, c'est un prompt.
+echo ("Username ?");
+read $username;
+echo ("Password ?");
+read $password;
+
+prosodyctl adduser $username"@unicom.itinet.fr"
